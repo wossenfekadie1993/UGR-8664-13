d25f3ddf3cf92721e587353cfac73c0fe535332d
1234yabu <119517823+1234yabu@users.noreply.github.com>
2023-01-18 00:19:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,16 +8,16 @@ hamburgur.onclick=function(){
 /*for homepage*/
 function changeBg(){
     const image=[
-        'url("../images/jordan3.png")',
-        'url("../images/nike5.png")',
-        'url("../images/puma7.png")',
-        'url("../images/jordan6.png")',
-        'url("../images/nike3.png")',
-        'url("../images/jordan4.png")',
-        'url("../images/nike6.png")',
-        'url("../images/jordan5.png")',
-        'url("../images/puma6.png")',
-        'url("../images/puma3.png")',
+        'url("jordan3.png")',
+        'url("nike5.png")',
+        'url("puma7.png")',
+        'url("jordan6.png")',
+        'url("nike3.png")',
+        'url("jordan4.png")',
+        'url("nike6.png")',
+        'url("jordan5.png")',
+        'url("puma6.png")',
+        'url("puma3.png")',
     ]
     const shoeImage=document.querySelector(".shoe-background")
     const bg=image[Math.floor(Math.random()*image.length)];
